10995d12a820ffe1e1c2cece4e3cd24b37b390e8
Izabela <izabelagoes1@gmail.com>
2021-05-21 20:19:28 -0300

    Atividade

---
+++ src/main/java/br/com/constant/Constant.java
@@ -0,0 +1,12 @@
+package br.com.constant;
+
+public class Constant {
+
+    public static final String API_URL = "controlecompravenda/api/v1/";
+    public static final String API_CLIENTE =  API_URL + "cliente";
+    public static final String API_VENDA =  API_URL + "venda";
+    public static final String API_PRODUTO =  API_URL + "produto";
+    public static final String API_COMPRA =  API_URL + "compra";
+    public static final String API_FORNECEDOR =  API_URL + "fornecedor";
+}
+
